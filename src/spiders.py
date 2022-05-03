9ef144830e086e7402429274cdb998a2cfcca14f
Endre Farkas Kiss <fernstudiumva@protonmail.com>
2022-05-03 09:36:30 +0200

    Hetyetye

--- src/spiders.py
+++ src/spiders.py
@@ -5,7 +5,7 @@ from scrapy import Request, Spider
 
 class FanForumSpider(Spider):
     allowed_domains = ['sodika.org']
-    autothrottle_enabled=True
+    autothrottle_enabled = True
     name = 'FanForum'
 
     def start_requests(self):
