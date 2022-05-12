8f577463613e07f1d0637db2e3e9e6c2f790d17b
Endre Farkas Kiss <fernstudiumva@protonmail.com>
2022-05-12 18:23:51 +0200

    Replaced quote string with model

--- fanforum_crawler/items.py
+++ fanforum_crawler/items.py
@@ -4,11 +4,16 @@ from typing import Optional
 from pydantic import BaseModel
 
 
+class Quote(BaseModel):
+    author: str
+    content: Optional[str]
+
+
 class Comment(BaseModel):
     author: str
     created_at: datetime
     votes: int
-    quote: Optional[str]
+    quote: Optional[Quote]
     content: Optional[str]
     title: str
     signature: Optional[str]
--- fanforum_crawler/spiders/fanforum_spider.py
+++ fanforum_crawler/spiders/fanforum_spider.py
@@ -2,12 +2,13 @@ import pendulum
 from scrapy import Request, Spider
 from scrapy.http import HtmlResponse
 
-from fanforum_crawler.items import Comment
+from fanforum_crawler.items import Comment, Quote
 from fanforum_crawler import string_utils
 
 
 class FanForumSpider(Spider):
     NUMBER_OF_PAGES_TO_CRAWL = 1
+    XPATH_TEXT = './text()'
 
     allowed_domains = ['sodika.org']
     name = 'fanforumspider'
@@ -25,16 +26,24 @@ class FanForumSpider(Spider):
 
     def parse(self, response: HtmlResponse, **kwargs):
         for comment in response.xpath('//div[contains(@class, "comment")]'):
+            content_selector = comment.xpath('./div[@class="content"]//div[@class="innerDiv"]')
+            content = string_utils.join(content_selector.xpath(self.XPATH_TEXT).extract()) if content_selector else None
+            quote_selector = comment.xpath('./div[@class="content"]//div[@class="quote"]')
+            quote = Quote(author=quote_selector.xpath('./div[@class="author"]/text()').extract()[0],
+                          content=string_utils.join(
+                              quote_selector.xpath(self.XPATH_TEXT).extract())) if quote_selector else None
+            signature_selector = comment.xpath('./div[@class="content"]//i[@class="signature"]')
+            signature = string_utils.join(
+                signature_selector.xpath(self.XPATH_TEXT).extract()) if signature_selector else None
+
             yield Comment(
                 author=comment.xpath('./div[@class="header"]//strong/text()').get(),
                 created_at=pendulum.from_format(
                     comment.xpath('./div[@class="header"]//span[@class="date"]/text()').get(), 'MMM DD, YYYY HH:mm:ss',
                     tz='Europe/Budapest'),
                 votes=int(comment.xpath('./div[@class="header"]//b[starts-with(@class,"votes-")]/text()').get()),
-                quote=string_utils.join(comment.xpath('./div[@class="content"]//div[@class="quote"]/text()').extract()),
-                content=string_utils.join(comment.xpath('./div[@class="content"]//div[@class="innerDiv"]/text()')
-                                          .extract()),
+                quote=quote,
+                content=content,
                 title=comment.xpath('./div[@class="header"]//span/@title').get(),
-                signature=string_utils.join(comment.xpath('./div[@class="content"]//i[@class="signature"]/text()')
-                                            .extract())
+                signature=signature
             ).dict()
